4c28cbc096158a8ade40c404d203e091d717f885
gmccannon <gwm13@uakron.edu>
2023-12-27 17:52:53 -0500

    Configue docker files

--- src/App.js
+++ src/App.js
@@ -2,32 +2,24 @@
 import React, { useState } from 'react';
 import SearchForm from './components/SearchForm';
 import WeatherDisplay from './components/WeatherDisplay';
-import ForecastDisplay from './components/ForecastDisplay';
 import DefaultPage from './components/DefaultPage';
 
 
 const App = () => {
   const [weather, setWeather] = useState(null);
-  const [forecast, setForecast] = useState(null);
 
   const searchWeather = async (city) => {
     try {
       const apiKey = process.env.REACT_APP_API_KEY;
       
       const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
-      const apiUrl2 = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`;
 
       // Fetch current weather data
       const response = await fetch(apiUrl);
       const currentWeatherData = await response.json();
 
-      // Fetch forecast data
-      const forecastResponse = await fetch(apiUrl2);
-      const forecastData = await forecastResponse.json();
-
-      // Update state with both current weather and forecast data
+      // Update state with current weather
       setWeather(currentWeatherData);
-      setForecast(forecastData);
 
     } catch (error) {
       console.error('Error fetching weather data:', error);
